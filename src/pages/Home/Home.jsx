0cf257af10bd2a5c1b47427c04810f784eff5038
Alwaz <alwazkazi3@gmail.com>
2022-03-19 09:23:21 -0700

    all routes fixed

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return <div className="home">Home Page</div>;
+};
+
+export default Home;
---
+++ src/pages/SinglePage/SinglePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SinglePage = () => {
+  return <div>SinglePage Component</div>;
+};
+
+export default SinglePage;
