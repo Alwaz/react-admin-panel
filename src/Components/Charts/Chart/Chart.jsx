fbc9bf6b13caf9f73d7dbf342db052ff277e2e65
Alwaz <alwazkazi3@gmail.com>
2022-04-12 10:29:17 -0700

    chart component complete

--- src/Components/Charts/Chart/Chart.jsx
+++ src/Components/Charts/Chart/Chart.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./Chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Chart = () => {
-  return <div className="chart">Chart</div>;
+  const data = [
+    { name: "January", total: 700 },
+    { name: "Feburary", total: 2000 },
+    { name: "March", total: 800 },
+    { name: "April", total: 1600 },
+    { name: "May", total: 900 },
+    { name: "June", total: 1700 },
+    { name: "July", total: 500 },
+    { name: "August", total: 300 },
+    { name: "Sept", total: 1500 },
+  ];
+
+  return (
+    <div className="chart">
+      <div className="title">Last (9 months) Revenue</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 20, right: 30, left: 30, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="total"
+            stroke="#b9b9b9"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Chart;
