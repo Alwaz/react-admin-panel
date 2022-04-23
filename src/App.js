b82973fba0c23438b744a5923d72b352841de098
Alwaz <alwazkazi3@gmail.com>
2022-04-23 11:16:44 -0700

    new user page in progress

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import List from "./pages/List/List";
 import New from "./pages/New/New";
 import SinglePage from "./pages/SinglePage/SinglePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { userInputs, productInputs } from "../src/formSource";
 
 function App() {
   return (
@@ -16,7 +17,19 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<SinglePage />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productsId" element={<SinglePage />} />
+              <Route
+                path="new"
+                element={<New input={productInputs} title="Add New Product" />}
+              />
             </Route>
           </Route>
         </Routes>
--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -2,14 +2,45 @@ import React from "react";
 import "./New.scss";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Navbar from "../../Components/Navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
-const New = () => {
+const New = ({ inputs, title }) => {
   return (
     <div className="new">
       <Sidebar />
 
       <div className="new__container">
         <Navbar />
+
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+
+              {inputs.map((input) => {
+                return (
+                  <div className="formInput">
+                    <label>{input.label}</label>
+                    <input type={input.type} placeholder={input.placeholder} />
+                  </div>
+                );
+              })}
+
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
